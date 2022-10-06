8c254716ae272cfaa756b1f1310a4a6ded44c49d
jllaveherr <113448119+jllaveherr@users.noreply.github.com>
2022-10-06 10:58:38 +0200

    hecho hasta el 12

---
+++ ejercicio8.py
@@ -0,0 +1,6 @@
+n = int(input("intruce el primer numero"))
+m = int(input("introduce el primer numero"))
+print("el cociente es")
+print("n // m")
+print("el resto es")
+print(n % m)
---
+++ ejercicio9.py
@@ -0,0 +1,4 @@
+capital = float(input("canntidad que desea invertir"))
+interes = float(input("interes anueal"))
+anual = float(input("duracion de la inversion anual"))
+print(((capital * interes * anual)/100) + capital)
